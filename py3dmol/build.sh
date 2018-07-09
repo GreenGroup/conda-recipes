40866ada3a585f7989e2a9d39b5e982434dfafc0
Mark Payne <ampayne@mit.edu>
2018-07-09 17:17:41 -0400

    Add Conda recipe for py3Dmol

---
+++ py3dmol/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pip install --no-deps https://files.pythonhosted.org/packages/f8/8a/4cf31928a03b13f30e8b00134565990331087145f6e5248fd1bd7dcc2631/py3Dmol-0.7.2-py2.py3-none-any.whl
+
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
