dccf1abc4c278445482b65e8c64dda0352fa5cc9
Matt Johnson <mjohnson541@gmail.com>
2020-05-12 18:05:17 -0400

    semi-working pyjulia

--- pyjulia/build.sh
+++ pyjulia/build.sh
@@ -1,2 +1,4 @@
-julia -e "using Pkg; Pkg.add(\"PyCall\"); pypath =\"`which python`\"; ENV[\"CONDA_JL_HOME\"] = join(split(pypath,\"/\")[1:end-2],\"/\"); ENV[\"PYTHON\"] = pypath; Pkg.build(\"PyCall\"); using PyCall"
 python -m pip install --no-deps --ignore-installed .
+python -c "import julia; julia.install();"
+var=$(awk 'FNR==1{print $0 "3";}' $(which python-jl))
+sed -i ".bak" "1s|.*|$var|" $(which python-jl) #append 3 so python-jl works after linking
