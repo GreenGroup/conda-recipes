a956d7f04411f335cb8a4c8c5c1323ee4d2df693
Matt Johnson <mjohnson541@gmail.com>
2020-05-13 14:14:21 -0700

    get to work with linux

--- pyjulia/build.sh
+++ pyjulia/build.sh
@@ -1,4 +1,13 @@
 python -m pip install --no-deps --ignore-installed .
-python -c "import julia; julia.install();"
+julia -e "using Pkg; ENV[\"PYTHON\"]=string(ENV[\"PREFIX\"],\"/bin/python\"); println(ENV[\"PYTHON\"]); Pkg.build(\"PyCall\");"
+python -c "import julia; julia.install()"
+
 var=$(awk 'FNR==1{print $0 "3";}' $(which python-jl))
-sed -i ".bak" "1s|.*|$var|" $(which python-jl) #append 3 so python-jl works after linking
+unamestr=$(uname)
+if [[ "$unamestr" == "Linux" ]]; then
+    sed -i 's|bin/python|bin/python3|g' $(which python-jl)
+    #sed -i "1s|.*|$var|" $(which python-jl) #append 3 so python-jl works after linking
+elif [[ "$unamestr" == "Darwin" ]]; then
+    sed -i ".bak" 's|bin/python|bin/python3|g' $(which python-jl)
+    #sed -i ".bak" "1s|.*|$var|" $(which python-jl) #append 3 so python-jl works after linking
+fi
