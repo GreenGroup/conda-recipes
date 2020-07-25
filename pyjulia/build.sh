82ff074a2aeb57ee19ed7e349c8167e6144f053c
Matt Johnson <mjohnson541@gmail.com>
2020-07-25 12:58:53 -0400

    remove julia reconfiguring from pyjulia

--- pyjulia/build.sh
+++ pyjulia/build.sh
@@ -1,31 +1,5 @@
 python -m pip install --no-deps --ignore-installed .
 
-#set JULIA_DEPOT_PATH in conda env
-export JULIA_DEPOT_PATH="${PREFIX}/share/julia/site:$JULIA_DEPOT_PATH"
-ACTIVATE_ENV="${PREFIX}/etc/conda/activate.d/env_vars.sh"
-DEACTIVATE_ENV="${PREFIX}/etc/conda/deactivate.d/env_vars.sh"
-if [ -f "$ACTIVATE_ENV" ]; then
-        echo "export JULIA_DEPOT_PATH=\"${PREFIX}/share/julia/site:\$JULIA_DEPOT_PATH\"" >> "$ACTIVATE_ENV"
-else
-        mkdir -p ${PREFIX}/etc/conda/activate.d
-        touch ${PREFIX}/etc/conda/activate.d/env_vars.sh
-        echo '#!/bin/sh' >> $ACTIVATE_ENV
-        echo "export JULIA_DEPOT_PATH=\"${PREFIX}/share/julia/site:\$JULIA_DEPOT_PATH\"" >> "$ACTIVATE_ENV"
-fi
-if [ -f "$DEACTIVATE_ENV" ]; then
-        echo "unset JULIA_DEPOT_PATH" >> "$DEACTIVATE_ENV"
-else
-        mkdir -p ${PREFIX}/etc/conda/deactivate.d
-        touch ${PREFIX}/etc/conda/deactivate.d/env_vars.sh
-        echo '#!/bin/sh' >> "$DEACTIVATE_ENV"
-        echo "unset JULIA_DEPOT_PATH" >> "$DEACTIVATE_ENV"
-fi
-
-mkdir $PREFIX/share/julia/site
-mkdir $PREFIX/share/julia/site/registries 
-mkdir $PREFIX/share/julia/site/registries/General
-git clone https://github.com/JuliaRegistries/General.git $PREFIX/share/julia/site/registries/General
-
 #python -c "import julia; julia.install()"
 
 #rm -rf $PREFIX/share/julia/site/compiled/*
